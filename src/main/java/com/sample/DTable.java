851b11fbf3a45a6795aeebcd3c49360810de96f2
kjin <kjin@redhat.com>
2018-05-30 00:53:38 +0900

    modified DTable getRuleInfo

--- src/main/java/com/sample/DTable.java
+++ src/main/java/com/sample/DTable.java
@@ -17,28 +17,34 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.drools.compiler.compiler.io.Resource;
+import org.drools.core.io.impl.FileSystemResource;
+import org.drools.decisiontable.InputType;
+import org.drools.decisiontable.SpreadsheetCompiler;
 import org.drools.decisiontable.parser.DefaultRuleSheetListener;
 import org.drools.decisiontable.parser.xls.ExcelParser;
+import org.drools.template.parser.DataListener;
+import org.drools.template.parser.TemplateDataListener;
 import org.jboss.logging.Logger;
 import org.jboss.logging.Logger.Level;
 
 public class DTable {
+	static final String RULETABLE = "RuleTable";
+	static final String CONDITION = "CONDITION";
 
-	public static void main(String[] args) {
-		final String RULETABLE = "RuleTable";
-		final String CONDITION = "CONDITION";
-		String filepath = "src/main/resources/dtables/DealerClasificationRule.xlsx";
-		File file = new File(filepath);
+	public static Map<String, RuleInfo> getRuleInfo(File file) {
+		Map<String, RuleInfo> conditionColumns = new HashMap<String, RuleInfo>();
 
-    	DefaultRuleSheetListener listener = new DefaultRuleSheetListener();
-    	ExcelParser ep = new ExcelParser(listener);
-    	ep.parseFile(file);
-    	
-    	try {
-			Workbook workbook = WorkbookFactory.create(file);
+		Logger logger = Logger.getLogger(DTable.class.getName());
+
+ 			Workbook workbook = null;
+			try {
+				workbook = WorkbookFactory.create(file);
+			} catch (EncryptedDocumentException | InvalidFormatException | IOException e) {
+				e.printStackTrace();
+			}
 			Sheet sheet = workbook.getSheetAt(0);
 			
-			Map<String, RuleInfo> conditionColumns = new HashMap<String, RuleInfo>();
 			String ruleTableName = "";
 			
 			int rowNum = sheet.getLastRowNum();
@@ -72,6 +78,7 @@ public class DTable {
 			for(RuleInfo rule : conditionColumns.values()) {
 				for (int i = rule.getStartRow(); i <= rule.getLastRow(); i++) {
 					Row row = sheet.getRow(i);
+					if ( row == null ) continue;
 					for (Integer n : rule.getConditionColumns()) {
 						Cell cell = row.getCell(n);
 						String value = cell.getStringCellValue();
@@ -81,32 +88,36 @@ public class DTable {
 							map.put(Integer.toString(n), new ArrayList<String>(Arrays.asList("")));
 						}
 						List<String> list = map.get(Integer.toString(n));
-						list.add(value);
+						if ( !list.contains(value) )
+							list.add(value);
 						map.put(Integer.toString(n), list);
-						//System.out.println(i + " : " + n + " >> " + cell.getStringCellValue());
 					}
 				}
 			}
-			System.out.println(conditionColumns);
+			logger.log(Level.INFO, conditionColumns);
+			return conditionColumns;
+	}
+	
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(DTable.class.getName());
+
+		String filepath = "src/main/resources/dtables/DealerClasificationRule.xlsx";
+		// String filepath = "src/main/resources/dtables/Sample.xls";
+		// String filepath = "src/main/resources/dtables/IncentiveDetailRule.xlsx";
+
+		Map<String, RuleInfo> conditionColumns = getRuleInfo(new File(filepath));		
 			
-			Logger logger = Logger.getLogger(ProduceTestCase.class.getName());
-			for (RuleInfo rule : conditionColumns.values()) {
-				Map<String, List<String>> map = rule.getMap();
-				logger.log(Level.INFO, rule.getRuleName());
+		for (RuleInfo rule : conditionColumns.values()) {
+			logger.log(Level.INFO, rule);
+			Map<String, List<String>> map = rule.getMap();
 				
-				List<Supplier<Stream<String>>> list = new ArrayList<Supplier<Stream<String>>>();
-				//map.keySet().stream().forEach(System.out::println);
-				map.values().stream().forEach(l -> {
-					list.add(()->l.stream());
-				});
-				Cartesian.go((a, b) -> a + "," + b, list)
-								.forEach(System.out::println);
-			}
-			
-		} catch (EncryptedDocumentException | InvalidFormatException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			List<Supplier<Stream<String>>> list = new ArrayList<Supplier<Stream<String>>>();
+			//map.keySet().stream().forEach(System.out::println);
+			map.values().stream().forEach(l -> {
+				list.add(()->l.stream());
+			});
+			Cartesian.go((a, b) -> a + "," + b, list)
+							.forEach(System.out::println);
 		}
 	}
-
 }
--- src/main/java/com/sample/ProduceTestCase.java
+++ src/main/java/com/sample/ProduceTestCase.java
@@ -1,5 +1,6 @@
 package com.sample;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,27 +13,25 @@ import org.jboss.logging.Logger.Level;
 
 public class ProduceTestCase {
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		//String filepath = "src/main/resources/dtables/DealerClasificationRule.xlsx";
+		String filepath = "src/main/resources/dtables/Sample.xls";
 		Logger logger = Logger.getLogger(ProduceTestCase.class.getName());
 		
-		try {
-			Map<String, List<String>> map = CsvReader.CsvToMap("sample.csv");
+		Map<String, RuleInfo> conditionColumns = DTable.getRuleInfo(new File(filepath));
+		
+		for (RuleInfo ruleInfo : conditionColumns.values()) {
+			Map<String, List<String>> map = ruleInfo.getMap();
 			logger.log(Level.INFO, map);
 						
 			List<Supplier<Stream<String>>> list = new ArrayList<Supplier<Stream<String>>>();
 			
-			map.keySet().stream().forEach(System.out::println);
+			//map.keySet().stream().forEach(System.out::println);
 			map.values().stream().forEach(l -> {
 				list.add(()->l.stream());
 			});
 			Cartesian.go((a, b) -> a + "," + b, list)
 							.forEach(System.out::println);
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
-
 }
